10a796ec3213a26c52349e589256f6e2d60a8112
imattau <imatt.au@protonmail.com>
2026-06-01 21:07:30 +1000

    feat: add core TypeScript types

---
+++ src/types/index.ts
@@ -0,0 +1,35 @@
+export interface Comic {
+  id: string
+  pubkey: string
+  dTag: string
+  title: string
+  author: string
+  description: string
+  coverHash: string
+  blossomServer: string
+  tags: string[]
+  eventId: string
+}
+
+export interface Chapter {
+  id: string
+  pubkey: string
+  dTag: string
+  parentDTag: string
+  title: string
+  pageHashes: string[]
+  blossomServer: string
+  publishedAt: number
+  eventId: string
+}
+
+export interface ReadingProgress {
+  id: string
+  chapterDTag: string
+  page: number
+  updatedAt: number
+}
+
+export interface BlossomServer {
+  url: string
+}
